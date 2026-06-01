3f42d6a232b681e66cff93467a7cfaaaf731bf26
bit-pulse-3 <akbarazain22@gmail.com>
2026-06-01 09:59:51 +0500

    Hello Friends

---
+++ i.py
@@ -0,0 +1,11 @@
+def get_number():
+    try:
+        a=int(input("Enter a number: "))
+        return a
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+        return get_number()
+    
+
+if __name__ == "__main__":
+    print(get_number())
