c43dcb7ca5e811071547c0f19114d8fd93adf00a
bit-pulse-3 <akbarazain22@gmail.com>
2026-06-05 10:40:48 +0500

    Multiple of two

--- i.py
+++ i.py
@@ -5,9 +5,17 @@ def get_number():
     except ValueError:
         print("Invalid input. Please enter a valid number.")
         return get_number()
+
+# pyrefly: ignore [parse-error]
+def get_number_as_multiple_2():
+    a=get_number()
+    if a%2==0:
+        return a
+    else:
+        return get_number_as_multiple_2()
     
 
 if __name__ == "__main__":
-    print(get_number())
-    print(get_number())
-    print(get_number())
+    print(get_number_as_multiple_2())
+    print(get_number_as_multiple_2())
+    print(get_number_as_multiple_2())
