32b81ce8c7e205ac9c9c6a05b102472618890c03
bit-pulse-3 <akbarazain22@gmail.com>
2026-06-05 11:08:56 +0500

    Check Numbers if even or odd and check the number a multiple of number and get the table of the number

--- index.py
+++ index.py
@@ -11,3 +11,10 @@ def check_number_as_multiple(a):
     for i in range(a):
         if i%a==0:
             print(i)
+def get_table_of_a():
+    for i in range(10):
+        print(a*i)
+
+print(f"table of {a}:{get_table_of_a()}")
+print(f"number {a} is {check(a)}")
+print(f"{a} is a multiple of:{check_number_as_multiple(a)}") 
