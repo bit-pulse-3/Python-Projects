98ff793ecc5bc1a94056465a40cc99be3f526416
bit-pulse-3 <akbarazain22@gmail.com>
2026-06-08 11:19:38 +0500

    Print Message

--- index.py
+++ index.py
@@ -12,7 +12,9 @@ def check_number_as_multiple(a):
         if i%a==0:
             print(i)
 
-
+def printmessage():
+    print("hello")
+    
 
 print(f"number {a} is {check(a)}")
 print(f"{a} is a multiple of:{check_number_as_multiple(a)}") 
